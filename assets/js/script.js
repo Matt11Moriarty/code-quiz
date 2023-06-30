f3ae972957cc8b09f731e8cbb252014b21dacc91
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-06-30 14:48:48 -0700

    added queryselector vars

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+var questionVal = docuement.querySelector('#question');
+var answer1     = docuement.querySelector('#answer1');
+var answer2     = docuement.querySelector('#answer2');
+var answer3     = docuement.querySelector('#answer3');
+var answer4     = docuement.querySelector('#answer4');
+
