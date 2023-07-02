95ff324d2f2328e2eee85d8ebb642c8305e6b8a7
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-07-02 16:48:15 -0700

    framed out html and starting Css

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # code-quiz
+
+resources
+
+- project scope help
+  - https://www.youtube.com/watch?v=riDzcEQbX6k&t=76s
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 
-var questionVal = docuement.querySelector('#question');
-var answer1     = docuement.querySelector('#answer1');
-var answer2     = docuement.querySelector('#answer2');
-var answer3     = docuement.querySelector('#answer3');
-var answer4     = docuement.querySelector('#answer4');
+var questionVal = document.querySelector('#question');
+var answer1     = document.querySelector('#answer1');
+var answer2     = document.querySelector('#answer2');
+var answer3     = document.querySelector('#answer3');
+var answer4     = document.querySelector('#answer4');
 
+
+
+
+function startQuiz () {
+
+}    
+
+function selectAnswer () {
+
+}
