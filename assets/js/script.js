8fb91fff3940977d7701fee6fa765f574f419e86
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2023-07-06 15:42:09 -0700

    uncommented all the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,56 +48,55 @@ var questions = [
         a3: ['Lot\'s of stuff', true],
         a4: ['JavaScript uses semicolons', false]
 
+    },     {
+        qText: 'What does DOM stand for?',
+        a1: ['Duane Orwell Monroe', false],
+        a2: ['Doctor of Medicine', false],
+        a3: ['Document Oriented Mode', false],
+        a4: ['Document Object Model', true]
+    },     {
+        qText: 'How do you end a setInterval method?',
+        a1: ['endTimer', false],
+        a2: ['clearInterval', true],
+        a3: ['stopPlease', false],
+        a4: ['You don\'t', false]
+    },     {
+        qText: 'When should you use querySelector instead of getElementById?',
+        a1: ['When using SQL', false],
+        a2: ['When you are iterating through an array', false],
+        a3: ['When you have no idea what to do', false],
+        a4: ['When you are getting something other than an element identified by an id', true] 
+    },     {
+        qText: 'Correct way to declare a variable:',
+        a1: ['var', false],
+        a2: ['const', false],
+        a3: ['let', false],
+        a4: ['All of the above', true]
+    },     {
+        qText: 'With what element do you link a js file to your html?',
+        a1: ['<script>', true],
+        a2: ['Hydrogen', false],
+        a3: ['<javascriptfilelinkgoeshere>', false],
+        a4: ['<link>', false]
+    },     {
+        qText: 'In js, a method is:',
+        a1: ['the same as a variable', false],
+        a2: ['Clifford Smith, Jr. (born March 2, 1971)', false],
+        a3: ['an object property containing a function definition', true],
+        a4: ['idk ask someone else', false]
+    },     {
+        qText: 'Which of these statements would select the first item in the array?',
+        a1: ['array.first', false],
+        a2: ['give.me.the.first.element.of.the.array', false],
+        a3: ['array[1]', false],
+        a4: ['array[0]', true]
+    },     {
+        qText: 'Which of these function declarations are recieving an input?',
+        a1: ['function(input) {}', true],
+        a2: ['function.input {}', false],
+        a3: ['function () {input}', false],
+        a4: ['None of the above', false]
     }
-    // ,     {
-    //     qText: 'What does DOM stand for?',
-    //     a1: ['Duane Orwell Monroe', false],
-    //     a2: ['Doctor of Medicine', false],
-    //     a3: ['Document Oriented Mode', false],
-    //     a4: ['Document Object Model', true]
-    // },     {
-    //     qText: 'How do you end a setInterval method?',
-    //     a1: ['endTimer', false],
-    //     a2: ['clearInterval', true],
-    //     a3: ['stopPlease', false],
-    //     a4: ['You don\'t', false]
-    // },     {
-    //     qText: 'When should you use querySelector instead of getElementById?',
-    //     a1: ['When using SQL', false],
-    //     a2: ['When you are iterating through an array', false],
-    //     a3: ['When you have no idea what to do', false],
-    //     a4: ['When you are getting something other than an element identified by an id', true] 
-    // },     {
-    //     qText: 'Correct way to declare a variable:',
-    //     a1: ['var', false],
-    //     a2: ['const', false],
-    //     a3: ['let', false],
-    //     a4: ['All of the above', true]
-    // },     {
-    //     qText: 'With what element do you link a js file to your html?',
-    //     a1: ['<script>', true],
-    //     a2: ['Hydrogen', false],
-    //     a3: ['<javascriptfilelinkgoeshere>', false],
-    //     a4: ['<link>', false]
-    // },     {
-    //     qText: 'In js, a method is:',
-    //     a1: ['the same as a variable', false],
-    //     a2: ['Clifford Smith, Jr. (born March 2, 1971)', false],
-    //     a3: ['an object property containing a function definition', true],
-    //     a4: ['idk ask someone else', false]
-    // },     {
-    //     qText: 'Which of these statements would select the first item in the array?',
-    //     a1: ['array.first', false],
-    //     a2: ['give.me.the.first.element.of.the.array', false],
-    //     a3: ['array[1]', false],
-    //     a4: ['array[0]', true]
-    // },     {
-    //     qText: 'Which of these function declarations are recieving an input?',
-    //     a1: ['function(input) {}', true],
-    //     a2: ['function.input {}', false],
-    //     a3: ['function () {input}', false],
-    //     a4: ['None of the above', false]
-    // }
 ]
 
 
